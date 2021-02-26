d79dd24a93f911a73f499938c7fd32a95faaf855
munderseth <marku@s2technologies.com>
2021-02-26 09:43:04 -0800

    scripts to post

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # gh.community.dispatch
 Issue with workflow and dispatch events for branching
+
+
+## References
+
+- [Create a workflow dispatch event](https://docs.github.com/en/rest/reference/actions#create-a-workflow-dispatch-event)
+- [Create a repository dispatch event](https://docs.github.com/en/rest/reference/repos#create-a-repository-dispatch-event)
--- repo.post.sh
+++ repo.post.sh
@@ -1,10 +1,10 @@
 
-gh_dispatch_url="https://api.github.com/repos/munderseth/gh.community.dispatch/actions/workflows/workflow.dispatch.yml/dispatches"
+gh_dispatch_url="https://api.github.com/repos/munderseth/gh.community.dispatch/dispatches"
 
 curl -X POST $gh_dispatch_url \
 -H 'Content-Type: application/json' \
 -H 'Accept: application/vnd.github.v3+json' \
 -H "Authorization: token $GH_TOKEN" \
 -d '{
-     "ref":"main"
+     "event_type":"test"
     }'
